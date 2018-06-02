886ec8cedf4a7de71e5a45f2b66bccedb2cfed2a
Wojtascythe <wojtas75666@gmail.com>
2018-06-02 10:01:04 +0200

    Algorytm min max, pozostałe funkcje, dopieszczenie szczegułów

--- main.cpp
+++ main.cpp
@@ -107,6 +107,7 @@ int spr_wygr_w(int plansza[sizee][sizee])//sprawdza czy spe³niono warunek wygran
 	if (k==0) return spr_wygr(plansza,1);
 	return k;
 }
+
 void gen_plansze(int plansza[sizee][sizee])//metoda ma wygenerowaæ planszê
 {
 	for (int i=0; i<sizee; i++)
@@ -128,12 +129,112 @@ void pokaz_plansze(int plansza[sizee][sizee])//metoda ma wyœwietliæ planszê
 	}
 }
 
+int minimax(int plansza[sizee][sizee], int p1, int p2, int deep)//metoda minimax
+{
+	int naj_wyn=-9999;
+    int wyn= 0;
 
+    if (spr_wygr_w(plansza) == p1) return 1000;
+    else if (spr_wygr_w(plansza) == p2) return -1000;
+    else if (deep==0) return punktacja(plansza,p1);
 
+    for (int i=0; i<sizee; i++)
+	{
+        for (int j=0; j<sizee; j++)
+		{
+            if (plansza[i][j]==0)
+			{
+                plansza[i][j]=p1;
+                wyn=-(minimax(plansza, p2, p1,deep-1));
+                plansza[i][j]=0;
+                if (wyn>=naj_wyn) naj_wyn = wyn;
+            }
+        }
+    }
 
+    if (naj_wyn == -9999 || naj_wyn == 0) return 0;
+    else if (naj_wyn < 0) return naj_wyn + 1;
+    else if (naj_wyn > 0) return naj_wyn - 1;
+}
 
+int naj_ru(int plansza[sizee][sizee], int p1, int p2)// metoda okreœlaj¹ca najlepszy ruch do wykonania
+{
+	int naj_wyn = -9999;
+    int naj_ru_wiersz = -9999;
+    int naj_ru_kolumna = -9999;
+    int wyn= 0;
+	for (int i=0; i<sizee; i++)
+	{
+        for (int j=0; j<sizee; j++)
+		{
+            if (plansza[i][j]==0)
+			{
+                plansza[i][j]=p1;
+                wyn=-(minimax(plansza, p2, p1,deep));
+                plansza[i][j]=0;
+                if (wyn>=naj_wyn)
+				{
+                    naj_wyn = wyn;
+                    naj_ru_wiersz = i;
+                    naj_ru_kolumna = j;
+                }
+            }
+        }
+    }
+    return (100*naj_ru_wiersz + naj_ru_kolumna);
+}
+
+void ruch(int plansza[sizee][sizee])//ruch gracza
+{
+	int wiersz,kolumna;
+	cout<<"Podaj kolumne i wiersz"<<endl;
+	while(1)
+	{
+		cout<<"Wiersz: ";
+		cin>>wiersz;
+		cout<<"Kolumna: ";
+		cin>>kolumna;
+		if (kolumna>=1 && kolumna<=sizee && wiersz>=1 && wiersz<=sizee && plansza[wiersz-1][kolumna-1]==0)
+		{
+			plansza[wiersz-1][kolumna-1]=1;
+			break;
+		}
+		else cout<<"Nie mozna wybrac tego pola, wybierz inne. Upewnij sie ze pole nie jest zajete."<<endl;
+	}
+}
+
+void ruchSI(int plansza[sizee][sizee])
+{
+	int move=naj_ru(plansza,-1,1);
+	int wier=move/100;
+	int kol=move%100;
+	plansza[wier][kol]=-1;
+}
 
 int main()
 {
-	
+	int plansza[sizee][sizee];
+	gen_plansze(plansza);
+	char ext;
+	int ruchy=0;
+
+	while(ruchy<(sizee*sizee))
+	{
+		pokaz_plansze(plansza);
+		if (ruchy%2==0) ruch(plansza);
+		else ruchSI(plansza);
+		ruchy++;
+		pokaz_plansze(plansza);
+		if(spr_wygr_w(plansza)!=0)
+		{
+			if (spr_wygr_w(plansza)==1) cout<<"Wygrales."<<endl;
+			else cout<<"Przegrales."<<endl;
+			ext=getch();
+			exit(0);
+		}
+	}
+	pokaz_plansze(plansza);
+	cout<<"Remis."<<endl;
+	ext=getch();
+	exit(0);
 }
